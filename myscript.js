e1842a7aa9e02a09b0721b5d5187131aeda7e6fe
ka <ka.kaosf@gmail.com>
2013-04-22 17:55:46 +0900

    finish lesson 01

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var mainCtrl = function($scope) {
+  $scope.tasks = [
+    {body: 'task1', done: false},
+    {body: 'task2', done: false},
+    {body: 'task3', done: true},
+    {body: 'task4', done: false},
+  ];
+};
